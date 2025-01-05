fbcf4079e52cc12a3ee3475c51fec3d2dd2b634e
Daniilka48 <WellDevil48@yandex.ru>
2025-01-05 03:26:07 +0300

    77 passing

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const numStr = n.toString();
+    
+  const possibleNumbers = [];
+
+  for (let i = 0; i < numStr.length; i++) {
+      const newNumStr = numStr.slice(0, i) + numStr.slice(i + 1);
+      possibleNumbers.push(Number(newNumStr));
+  }
+
+  return Math.max(...possibleNumbers);
 }
 
 module.exports = {
--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  let blockedColumns = new Set();
+
+  for (let row of matrix) {
+      for (let col = 0; col < row.length; col++) {
+          if (row[col] === 0) {
+              blockedColumns.add(col);
+          } else if (!blockedColumns.has(col)) {
+              sum += row[col];
+          }
+      }
+  }
+
+  return sum;
 }
 
 module.exports = {
